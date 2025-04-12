7316367ac640f8c5e52d11ef1c1858a23f761686
jimmy-sketch <jimmy163mail@163.com>
2025-04-12 09:49:41 +0800

    增加zlib，开始添加main中的代码

--- osu!sync.core/main.cpp
+++ osu!sync.core/main.cpp
@@ -1,16 +1,26 @@
 #include<iostream>
 #include<algorithm>
+#include<filesystem>
 #include"fsutils.windows.h"
 #include"network.utils.h"
 #include"stableExporter.h"
 using namespace std;
+namespace fs = std::filesystem;
 
-#define TEST
+#define else if elif
 
-int main()
+int main(int argc,char* argv[])
 {
-#ifdef TEST
-	auto songsList=
-#endif // TEST
-
+	if (argc == 5)
+	{
+		fs::path osuFolderPath;
+		string options,serverURL;
+		osuFolderPath = argv[1], options = argv[2];
+	}
+	else
+	{
+		cout << "ÓÃ·¨£º" << endl
+			<< "osu!sync.core.exe [osu-folder] [download|upload|export|import] [serverURL]" << endl;
+		return -1;
+	}
 }
